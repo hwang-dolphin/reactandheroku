974236542d552bf8516ca53a0357530933e24c88
Robert <robert@Robertos-MacBook-Pro.local>
2020-05-10 15:34:13 -0700

    two

---
+++ build/precache-manifest.4cf84b3f2b779754d718a6dcbff92041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a77dd693a434a95812979091de795e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b75e7e8d48556be61d",
+    "url": "/static/css/main.c66d932c.chunk.css"
+  },
+  {
+    "revision": "ac8937aa99c6aad53b1f",
+    "url": "/static/js/2.5fcd5f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fcd5f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b75e7e8d48556be61d",
+    "url": "/static/js/main.7699d4d5.chunk.js"
+  },
+  {
+    "revision": "4629f0fefd45a45abdf7",
+    "url": "/static/js/runtime-main.c69f3cf2.js"
+  }
+]);
